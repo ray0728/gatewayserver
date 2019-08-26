13983e4bc8bf93f50f8dde771bcb3aa448e24d79
ray0728 <51101661@qq.com>
2019-08-26 23:01:20 +0800

    support auto redirect from http to https

--- src/main/java/com/rcircle/service/gateway/GatewayApplication.java
+++ src/main/java/com/rcircle/service/gateway/GatewayApplication.java
@@ -1,13 +1,11 @@
 package com.rcircle.service.gateway;
 
-import com.rcircle.service.gateway.events.sink.NewsSink;
-import com.rcircle.service.gateway.events.sink.SmsSink;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.context.request.RequestContextListener;
 
@@ -16,7 +14,6 @@ import org.springframework.web.context.request.RequestContextListener;
 @EnableCircuitBreaker
 @EnableFeignClients
 public class GatewayApplication {
-
     @Bean
     public RequestContextListener requestContextListener() {
         return new RequestContextListener();

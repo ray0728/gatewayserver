218eed613ad70c4850d9ccfe8c97f21c172c5564
ray0728 <51101661@qq.com>
2019-08-27 06:35:43 +0800

    Update HttpsComponent.java

--- src/main/java/com/rcircle/service/gateway/config/HttpsComponent.java
+++ src/main/java/com/rcircle/service/gateway/config/HttpsComponent.java
@@ -12,9 +12,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HttpsComponent {
-    @Value("${server.port}")
-    private int server_port;
-
     @Bean
     public ServletWebServerFactory servletContainer() {
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
@@ -37,11 +34,9 @@ public class HttpsComponent {
     public Connector httpConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
-        //Connector监听的http的端口号
         connector.setPort(80);
         connector.setSecure(false);
-        //监听到http的端口号后转向到的https的端口号
-        connector.setRedirectPort(server_port);
+        connector.setRedirectPort(443);
         return connector;
     }
 }

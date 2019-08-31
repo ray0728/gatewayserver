3ea4882506fe29454d30c5b2207cd82e0068f44c
ray0728 <51101661@qq.com>
2019-08-31 21:54:47 +0800

    merge htts and sso

--- src/main/java/com/rcircle/service/gateway/clients/RemoteSsoClientConfiguration.java
+++ src/main/java/com/rcircle/service/gateway/clients/RemoteSsoClientConfiguration.java
@@ -1,11 +1,29 @@
 package com.rcircle.service.gateway.clients;
 
+import feign.Client;
+import feign.Feign;
 import feign.RequestInterceptor;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class RemoteSsoClientConfiguration extends HttpsConfiguration{
+public class RemoteSsoClientConfiguration {
+    @Value("${server.ssl.key-store}")
+    private String keyStore;
+    @Bean
+    public Feign.Builder feignBuilder() {
+        final Client trustSSLSockets = client();
+        return Feign.builder().client(trustSSLSockets);
+    }
+
+    @Bean
+    public Client client() {
+        return new Client.Default(
+                TrustingSSLSocketFactory.get(keyStore), new NoopHostnameVerifier());
+    }
+
     @Bean
     public RequestInterceptor OAuth2SsoRequestInterceptor() {
         return new RemoteSsoRequestInterceptor();

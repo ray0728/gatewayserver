9e93bfcf576cd1f01f962936af5eed99ecc27302
ray0728 <51101661@qq.com>
2019-09-11 08:44:11 +0800

    update docker file

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -8,25 +8,25 @@ echo "*******  Zipkin Server has started"
 echo "********************************************************"
 echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
 echo "********************************************************"
-while ! `nc -z configserver $CONFIGSERVER_PORT`; do sleep 3; done
+while ! `nc -z config.iamray.cn $CONFIGSERVER_PORT`; do sleep 3; done
 echo "*******  Configuration Server has started"
 
 echo "********************************************************"
 echo "Waiting for the eureka server to start on port $EUREKASERVER_PORT"
 echo "********************************************************"
-while ! `nc -z discoveryserver $EUREKASERVER_PORT`; do sleep 3; done
+while ! `nc -z discovery.iamray.cn $EUREKASERVER_PORT`; do sleep 3; done
 echo "******* Eureka Server has started"
 
 echo "********************************************************"
 echo "Waiting for the account server to start  on port $ACCOUNT_PORT"
 echo "********************************************************"
-while ! `nc -z accountserver $ACCOUNT_PORT`; do sleep 10; done
+while ! `nc -z account.iamray.cn $ACCOUNT_PORT`; do sleep 10; done
 echo "******* ACCOUNT has started"
 
 echo "********************************************************"
 echo "Waiting for the resource server to start  on port $RESOURCE_PORT"
 echo "********************************************************"
-while ! `nc -z resourceserver $RESOURCE_PORT`; do sleep 10; done
+while ! `nc -z resource.iamray.cn $RESOURCE_PORT`; do sleep 10; done
 echo "******* RESOURCE has started"
 
 

404f19330f7dd4385cd0e419c330fcacebc23dc1
ray0728 <37318939+ray0728@users.noreply.github.com>
2020-02-15 17:33:28 +0800

    对特定接口放行

---
+++ src/main/java/com/rcircle/service/gateway/utils/CsrfSecurityRequestMatcher.java
@@ -0,0 +1,21 @@
+package com.rcircle.service.gateway.security;
+
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.regex.Pattern;
+
+public class CsrfSecurityRequestMatcher implements RequestMatcher {
+    private Pattern allowedMethods = Pattern.compile("^(GET|HEAD|TRACE|OPTIONS)$");
+    private RegexRequestMatcher unprotectedMatcher = new RegexRequestMatcher("^/rst/lab/voice/device.*", HttpMethod.PUT.name());
+
+    @Override
+    public boolean matches(HttpServletRequest request) {
+        if(allowedMethods.matcher(request.getMethod()).matches()){
+            return false;
+        }
+        return !unprotectedMatcher.matches(request);
+    }
+
+}
